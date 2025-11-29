8dbf4515a4ecd665d52f2e26ace5736e473a59fc
mjvnaaa <dilan271594@gmail.com>
2025-11-29 13:17:16 +0700

    feat(backend): Implement MovieModelJevon with logic trap readiness

--- lib/models/movie_model_jevon.dart
+++ lib/models/movie_model_jevon.dart
@@ -1,3 +1,38 @@
 class MovieModelJevon {
-  
+  final String id;
+  final String title;
+  final String poster;
+  final int basePrice;
+  final double rating;
+  final int duration;
+
+  MovieModelJevon({
+    required this.id,
+    required this.title,
+    required this.poster,
+    required this.basePrice,
+    required this.rating,
+    required this.duration,
+  });
+
+  factory MovieModelJevon.fromMapJevon(Map<String, dynamic> data, String docId) {
+    return MovieModelJevon(
+      id: docId,
+      title: data['title'] ?? '',
+      poster: data['poster'] ?? '',
+      basePrice: (data['basePrice'] ?? 0).toInt(),
+      rating: (data['rating'] ?? 0.0).toDouble(),
+      duration: (data['duration'] ?? 0).toInt(),
+    );
+  }
+
+  Map<String, dynamic> toMapJevon() {
+    return {
+      'title': title,
+      'poster': poster,
+      'basePrice': basePrice,
+      'rating': rating,
+      'duration': duration,
+    };
+  }
 }
